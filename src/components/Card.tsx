54edc81e713780179f5561d0f344e025c57d750b
Jared Grimes <jaredlgrimes@hotmail.com>
2023-01-31 14:56:10 -0500

    hover lookin clean

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,15 @@
-import { Image } from '@chakra-ui/react';
-import { useCallback, useEffect, useState } from 'react';
+import { Box } from '@chakra-ui/react';
+import { MouseEvent, useCallback, useState } from 'react';
 
 const THRESHOLD = 15;
 
 export const Card = () => {
   const [transform, setTransform] = useState('');
+  const [glowBackgroundImage, setGlowBackgroundImage] = useState(
+    'radial-gradient(circle at 50% -20%, #ffffff22, #0000000f)'
+  );
 
-  const handleHover = useCallback(e => {
+  const handleHover = useCallback((e: MouseEvent<HTMLDivElement>) => {
     const { clientX, clientY, currentTarget } = e;
     const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
 
@@ -17,31 +20,57 @@ export const Card = () => {
     const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
 
     transformCard(clientWidth, rotateY, rotateX);
+    setGlowBackgroundImage(`radial-gradient(
+      circle at
+      ${horizontal * 2 + clientX / 2}px
+      ${vertical * 2 + clientY / 2}px,
+      #ffffff55,
+      #0000000f
+    )
+  `);
   }, []);
 
-  const resetStyles = useCallback(e => {
+  const resetStyles = useCallback((e: MouseEvent<HTMLDivElement>) => {
     setTransform(
-      `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
+      `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`
     );
   }, []);
 
   const transformCard = useCallback(
     (clientWidth: number, rotateY: string, rotateX: string) => {
       setTransform(
-        `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
+        `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1.1, 1.1, 1.1)`
       );
     },
     []
   );
 
   return (
-    <Image
+    <Box
       onMouseMove={handleHover}
       onMouseLeave={resetStyles}
       transform={transform}
+      transitionDuration='300ms'
+      transitionTimingFunction='ease-out'
       aria-label='Colress'
-      src='https://images.pokemontcg.io/swsh12pt5gg/GG59_hires.png'
-      height='300px'
-    />
+      backgroundImage='url(https://images.pokemontcg.io/swsh12pt5gg/GG59_hires.png)'
+      backgroundSize='cover'
+      height={`${250 * 1.396}px`}
+      width='250px'
+      borderRadius={13}
+      _hover={{
+        boxShadow: '0 5px 20px 5px #00000044;',
+      }}
+    >
+      <Box
+        position={'absolute'}
+        width='100%'
+        height='100%'
+        top={0}
+        left={0}
+        backgroundImage={glowBackgroundImage}
+        borderRadius={13}
+      />
+    </Box>
   );
 };
