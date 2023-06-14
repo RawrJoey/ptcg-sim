c463da693256781a89d7877471edf8cfcd2b5e96
BuildTools <unconfigured@null.spigotmc.org>
2023-06-14 17:04:20 -0500

    can add card to hand

--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -1,6 +1,7 @@
 import { HStack } from '@chakra-ui/react';
 import { CardInterface } from './Card/CardInterface';
 import { DraggableCard } from './Card/DraggableCard';
+import { DropZone } from './Generic/DropZone';
 
 interface HandProps {
   cards: CardInterface[];
@@ -8,17 +9,19 @@ interface HandProps {
 
 export const Hand = (props: HandProps) => {
   return (
-    <HStack maxWidth={'100%'} spacing={0} justifyContent='center'>
-      {props.cards.map((card) => (
-        <DraggableCard
-          card={card}
-          key={card.id}
-          hoverBehavior='float'
-          size='md'
-          entranceBehavior='draw'
-          cardOrigin='hand'
-        />
-      ))}
-    </HStack>
+    <DropZone zone='hand'>
+      <HStack maxWidth={'100%'} spacing={0} justifyContent='center'>
+        {props.cards.map((card) => (
+          <DraggableCard
+            card={card}
+            key={card.id}
+            hoverBehavior='float'
+            size='md'
+            entranceBehavior='draw'
+            cardOrigin='hand'
+          />
+        ))}
+      </HStack>
+    </DropZone>
   );
 };
