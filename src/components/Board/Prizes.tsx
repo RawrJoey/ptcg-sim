b1647834034a3e3d8f8d0a51508d75d1a63056ec
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 22:04:22 -0500

    prize key

--- src/components/Board/Prizes.tsx
+++ src/components/Board/Prizes.tsx
@@ -7,7 +7,7 @@ export const Prizes = () => {
 
   return (
     <Grid gridTemplateColumns='9rem 9rem' gridTemplateRows='1fr 1fr 1fr'>
-      {prizes.map((prizeCard) => <DraggableCard cardOrigin={{ area: 'prizes' }} card={prizeCard} size='md' hoverBehavior='float' isHidden />)}
+      {prizes.map((prizeCard) => <DraggableCard key={prizeCard.uuid} cardOrigin={{ area: 'prizes' }} card={prizeCard} size='md' hoverBehavior='float' isHidden />)}
     </Grid>
   )
 }
