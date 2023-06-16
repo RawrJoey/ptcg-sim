186dd238d46b0ba9b8290e042f26ec02040349d2
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 16:23:14 -0500

    update key

--- src/components/Game/GameController.tsx
+++ src/components/Game/GameController.tsx
@@ -10,7 +10,7 @@ export const GameController = () => {
   const activeGame = useActiveGame();
 
   useGameController();
-  useGameChannelListener();
+  useGameChannelListener(activeGame?.id);
   useGameChannelUpdater(activeGame?.id);
 
   return <>
--- src/features/game/broadcast/useGameChannelListener.ts
+++ src/features/game/broadcast/useGameChannelListener.ts
@@ -2,12 +2,12 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useEffect } from "react";
 import { GAMEPLAY_ACTION_EVENT } from "./types";
 
-export const useGameChannelListener = () => {
+export const useGameChannelListener = (challengeId: number | undefined) => {
   const supabase = useSupabaseClient();
 
   useEffect(() => {
     supabase
-    .channel('test')
+    .channel(`game-${challengeId}`)
     .on('broadcast', { event: GAMEPLAY_ACTION_EVENT }, (payload) => console.log(payload))
   }, [])
 };
