4dfbea717960ab944a391bbf87da5748b996e7aa
BuildTools <unconfigured@null.spigotmc.org>
2023-06-15 20:05:23 -0500

    fixed pokemon duplication

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -10,17 +10,18 @@ interface PokemonProps {
 
 export const Pokemon = (props: PokemonProps) => {
   const area = props.isActive ? 'active' : 'benched';
-  const zone: CardZone = { area: 'pokemon', metadata: props.card, parentArea: area };
+  const pokemonZone: CardZone = { area: 'pokemon', metadata: props.card, parentArea: area };
+  const activeBenchedZone: CardZone = { area, metadata: props.card };
 
   return (
-    <DropZone zone={zone}>
+    <DropZone zone={pokemonZone}>
       <Box>
         <>
           <Box>
-            <DraggableCard cardOrigin={zone} card={props.card} size={props.isActive ? 'lg' : 'md'} hoverBehavior='float' />
+            <DraggableCard cardOrigin={activeBenchedZone} card={props.card} size={props.isActive ? 'lg' : 'md'} hoverBehavior='float' />
           </Box>
           {props.card.energyAttached.map((energy) => (
-            <DraggableCard cardOrigin={zone} card={energy} size={props.isActive ? 'md' : 'sm'} hoverBehavior='float' />
+            <DraggableCard cardOrigin={pokemonZone} card={energy} size={props.isActive ? 'md' : 'sm'} hoverBehavior='float' />
           ))}
         </>
       </Box>
