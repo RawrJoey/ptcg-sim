411149aabdd033306f3d9c156705405271681e7a
Jared Grimes <jaredlgrimes@hotmail.com>
2023-06-15 22:26:44 -0500

    take prize by clicking on it

--- src/components/Board/Prizes.tsx
+++ src/components/Board/Prizes.tsx
@@ -1,13 +1,25 @@
-import { useAppSelector } from "@/app/hooks"
-import { Grid, HStack } from "@chakra-ui/react";
-import { DraggableCard } from "../Card/DraggableCard";
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { takePrize } from '@/features/game/gameSlice';
+import { Box, Grid, HStack } from '@chakra-ui/react';
+import { DraggableCard } from '../Card/DraggableCard';
 
 export const Prizes = () => {
-  const prizes = useAppSelector((state) => state.game.myDeck.prizes);
+  const prizes = useAppSelector(state => state.game.myDeck.prizes);
+  const dispatch = useAppDispatch();
 
   return (
     <Grid gridTemplateColumns='5rem auto'>
-      {prizes.map((prizeCard) => <DraggableCard key={prizeCard.uuid} cardOrigin={{ area: 'prizes' }} card={prizeCard} size='sm' hoverBehavior='float' isHidden />)}
+      {prizes.map((prizeCard, idx) => (
+        <Box key={prizeCard.uuid} onClick={() => dispatch(takePrize(idx))}>
+          <DraggableCard
+            cardOrigin={{ area: 'prizes' }}
+            card={prizeCard}
+            size='sm'
+            hoverBehavior='float'
+            isHidden
+          />
+        </Box>
+      ))}
     </Grid>
-  )
-}
+  );
+};
