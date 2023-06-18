8b9a85c500ea5d6508ff1a63bb669b45c5b9989d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-17 23:33:38 -0500

    lhgdf

--- src/features/game/broadcast/useGameChannelSubscribe.ts
+++ src/features/game/broadcast/useGameChannelSubscribe.ts
@@ -12,24 +12,17 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
   const supabase = useSupabaseClient();
   const channel = supabase.channel(`game-${challengeId}`);
   const myActions = useAppSelector((state) => state.game.gameplayActions);
-  const acks = useAppSelector((state) => state.game.acks);
   const currentPhase = useAppSelector((state) => state.game.phase);
 
   const myActionsRef: MutableRefObject<GameplayAction<any>[]> = useRef([])
   const myActionsStoredLength = useRef(0);
 
-  const acksRef: MutableRefObject<GamePhase[]> = useRef([]);
-  const acksLength = useRef(0);
 
   const currentPhaseRef: MutableRefObject<GamePhaseState | undefined> = useRef();
 
   useEffect(() => {
     myActionsRef.current = myActions;
   }, [myActions]);
-
-  useEffect(() => {
-    acksRef.current = acks;
-  }, [acks]);
 
   useEffect(() => {
     currentPhaseRef.current = currentPhase;
@@ -49,21 +42,12 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
             const lengthDiff = myActionsRef.current.length - myActionsStoredLength.current;
             myActionsStoredLength.current = myActionsRef.current.length;
 
+            console.log('sending',  myActionsRef.current.slice(myActionsRef.current.length - lengthDiff));
+
             channel.send({
               type: 'broadcast',
               event: GAMEPLAY_ACTION_EVENT,
               payload: myActionsRef.current.slice(myActionsRef.current.length - lengthDiff),
-            }).catch((err) => console.log(err));
-          }
-
-          if (acksLength.current < acksRef.current.length) {
-            const lengthDiff = acksRef.current.length - acksLength.current;
-            acksLength.current = acksRef.current.length;
-
-            channel.send({
-              type: 'broadcast',
-              event: GAME_ACK_EVENT,
-              payload: acksRef.current.slice(acksRef.current.length - lengthDiff),
             }).catch((err) => console.log(err));
           }
         }, 500);
