4acd21bce2fa9110679303fe86af874620c5335c
BuildTools <unconfigured@null.spigotmc.org>
2023-06-21 16:25:22 -0500

    made page controller on home

--- src/pages/cards.tsx
+++
@@ -1,5 +0,0 @@
-import { PageController } from '@/components/PageController';
-
-export default function Cards() {
-  return <PageController />
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Heading, Stack, Text } from '@chakra-ui/react';
+import { PageController } from '@/components/PageController';
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <Stack height='100%' justifyContent={'center'} align='center'>
-      <Heading size='2xl'>The new PTCG Live.</Heading>
-      <Text fontSize='xl'>{`Coming April. Probably.`}</Text>
-    </Stack>
+    <PageController />
   );
 }
