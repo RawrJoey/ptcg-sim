8c12c1714ece40f1181ba45908b7d8f9e07d7095
BuildTools <unconfigured@null.spigotmc.org>
2023-06-26 10:25:31 -0500

    fix beta flag

--- src/pages/app_test.tsx
+++ src/pages/app_test.tsx
@@ -1,13 +1,16 @@
 import { PageController } from '@/components/PageController';
 import { Inter } from '@next/font/google';
-import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // BETA FLAG - REMOVE
-  const router = useRouter();
-  if (router.basePath === 'twinleaf.gg') return;
+  const origin =
+  typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : '';
+
+  if (origin.includes('twinleaf.gg')) return;
 
   return (
     <PageController />
