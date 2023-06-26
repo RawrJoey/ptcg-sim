d647ecfd3957f27b7bc3711c42a55b780910c1fd
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 21:59:10 -0500

    trying to make the channels not hang randomly

--- src/features/game/broadcast/useGameChannelSubscribe.ts
+++ src/features/game/broadcast/useGameChannelSubscribe.ts
@@ -6,7 +6,7 @@ import { GamePhase, GamePhaseState, GameState } from "../types/Game";
 import { GameplayAction } from "../types/GameplayActions";
 import { GAMEPLAY_ACTION_EVENT, GAME_ACK_EVENT } from "./types";
 import { useIncomingActionHandler } from "./useIncomingActionHandler";
-import type { RealtimePresenceState } from '@supabase/supabase-js';
+import type { RealtimePresenceState, RealtimeChannel } from '@supabase/supabase-js';
 
 export const useGameChannelSubscribe = (challengeId: number | undefined) => {
   const dispatch = useAppDispatch();
@@ -16,13 +16,8 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
   const supabase = useSupabaseClient();
 
   const broadcastChannel = supabase.channel(`game-${challengeId}`);
-  const presenceChannel = supabase.channel(`game-${challengeId}-presence`, {
-    config: {
-      presence: {
-        key: user?.id,
-      },
-    },
-  });
+  console.log(user?.id)
+  let presenceChannel: RealtimeChannel | undefined;
 
   const myActions = useAppSelector((state) => state.game.gameplayActions);
   const currentPhase = useAppSelector((state) => state.game.phase);
@@ -47,44 +42,23 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
   }, [gameState]);
 
   useEffect(() => {
+    if (user?.id) {
+      presenceChannel = supabase.channel(`game-${challengeId}-presence`, {
+        config: {
+          presence: {
+            key: user?.id,
+          },
+        },
+      });
+    }
+  }, [user?.id]);
+
+  useEffect(() => {
     if (!challengeId) return;
+    if (presenceChannel === undefined) return;
 
-    // broadcastChannel.on('broadcast', { event: GAMEPLAY_ACTION_EVENT }, (event) => {
-    //   for (const action of event.payload) {
-    //     gameplayActionHandler({ type: action.type, payload: action.payload })
-    //   }
-    // }).subscribe((status) => {
-    //   if (status === 'SUBSCRIBED') {
-    //     setInterval(() => {
-    //       if (myActionsStoredLength.current < myActionsRef.current.length) {
-    //         console.log('sending',  myActionsRef.current[myActionsStoredLength.current]);
-    //         broadcastChannel.send({
-    //           type: 'broadcast',
-    //           event: GAMEPLAY_ACTION_EVENT,
-    //           payload: [myActionsRef.current[myActionsStoredLength.current]],
-    //         }).catch((err) => console.log(err));
-
-    //         myActionsStoredLength.current += 1;
-    //       }
-    //     }, 201);
-
-    //     // If current phase wasn't acked, try again
-    //     setInterval(() => {
-    //       if (currentPhaseRef.current?.status === 'ok' && currentPhaseRef.current?.acked === false) {
-    //         console.log(currentPhaseRef, 'failed')
-    //         console.log('Heartbeat failed. Trying again...');
-    //         broadcastChannel.send({
-    //           type: 'broadcast',
-    //           event: GAMEPLAY_ACTION_EVENT,
-    //           payload: [{ type: 'game/setGamePhase', payload: currentPhaseRef.current }]
-    //         }).catch((err) => console.log(err));
-    //       }
-    //     }, 2000);
-    //   }
-    // });
-    
-    presenceChannel.on('presence', { event: 'sync' }, () => {
-      const state: RealtimePresenceState<{ gameState: GameState }> = presenceChannel.presenceState();
+    presenceChannel!.on('presence', { event: 'sync' }, () => {
+      const state: RealtimePresenceState<{ gameState: GameState }> = presenceChannel!.presenceState();
       const opponentId = Object.keys(state).find((key) => key !== user?.id);
 
       if (!opponentId) {
@@ -100,7 +74,7 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
     }).subscribe((status) => {
       if (status === 'SUBSCRIBED') {
         setInterval(() => {
-          const state: RealtimePresenceState<{ gameState: GameState }> = presenceChannel.presenceState();
+          const state: RealtimePresenceState<{ gameState: GameState }> = presenceChannel!.presenceState();
 
           if (user?.id) {
             const myPresence = state[user.id];
@@ -115,13 +89,13 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
                 const somethingChanged = actionWasPerformed || phaseChanged;
 
                 if (somethingChanged) {
-                  presenceChannel.track({
+                  presenceChannel!.track({
                     gameState: gameStateRef.current
                   })
                 }
               }
             } else {
-              presenceChannel.track({
+              presenceChannel!.track({
                 gameState: gameStateRef.current
               })
             }
@@ -131,7 +105,7 @@ export const useGameChannelSubscribe = (challengeId: number | undefined) => {
     })
 
     return () => {
-      supabase.removeAllChannels();
+      presenceChannel && supabase.removeChannel(presenceChannel);
     };
-  }, []);
+  }, [presenceChannel === undefined]);
 }
