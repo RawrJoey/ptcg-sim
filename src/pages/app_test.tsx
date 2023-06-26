fc4d76f588583877672714eacc67b3effb2c4347
BuildTools <unconfigured@null.spigotmc.org>
2023-06-26 10:17:15 -0500

    added beta flag

--- src/pages/app_test.tsx
+++ src/pages/app_test.tsx
@@ -4,6 +4,9 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  // BETA FLAG - REMOVE
+  if (location.hostname === 'twinleaf.gg') return;
+
   return (
     <PageController />
   );
